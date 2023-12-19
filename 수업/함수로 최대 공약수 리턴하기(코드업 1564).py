1cb2ca9a1397fd49729a936f7aadf97a6e976839
박강원 <nambaggangwon@gmail.com>
2023-12-20 06:25:18 +0900

    유클리드 추가

--- 수업/함수로 최대 공약수 리턴하기(코드업 1564).py
+++ 수업/함수로 최대 공약수 리턴하기(코드업 1564).py
@@ -1,3 +1,4 @@
+
 a,b = map(int, input().split())
 
 while a%b !=0:
@@ -5,7 +6,8 @@ while a%b !=0:
 
 print(b)
 
-"""
+########################################################################
+
 def f(a,b):
     while(b!=0):
       a, b = b, a%b
@@ -13,4 +15,27 @@ def f(a,b):
 
 a, b = map(int, input().split())
 print(f(a,b))
-"""
+
+########################################################################
+
+a,b = map(int, input().split())
+
+def f(a,b):
+  if a%b == 0:# 나머지가 0인지 확인
+    return b
+  return f(b, a%b)#a%b != 0
+
+result = f(a,b)
+print(result)
+
+########################################################################
+#1번과 동일
+a,b = map(int, input().split())
+
+def gcd(a,b):
+  if a%b !=0: #나머지가 0이 아닌지 확인
+    return gcd(b, a%b)
+  return b#a%b == 0
+  
+result = gcd(a,b)
+print(result)
